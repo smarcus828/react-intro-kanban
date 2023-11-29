332a3029e3e68902a6e79e589301489620691ab1
Steven Marcus <smarcus@cicsnc.org>
2023-11-28 23:37:03 -0500

    done playing

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,48 @@
 import './App.css'
 
+const name="Steven Marcus";
+const button = <button>Click Me Fecker</button>;
+const bigRedCss = "text-4xl text-blue-600 border-red-900";
+
 function App() {
 
-  return (
-    <div className="text-4xl text-red-500">
-      Hello
-    </div>
-  )
+  const numThings = 3;
+
+  const wholeThing = <div className={bigRedCss}>
+    Hello {name} !!!
+  {button}
+  </div>
+
+  return numThings > 4 ? wholeThing : <div>404</div>
 }
 
 export default App
+
+// *** Weird because wholeThing doesn't use quotes ""
+// function App() {
+//   const name="Steven Marcus";
+//   const button = <button>Click Me</button>;
+//   const bigRedCss = "text-4xl text-green-600"
+//   const wholeThing = <div className={bigRedCss}>
+//     Hello {name}
+//   {button}
+//   </div>
+//     return wholeThing
+//   }
+
+// *** Another example with ternary return statement
+// const name="Steven Marcus";
+// const button = <button>Click Me Fecker</button>;
+// const bigRedCss = "text-4xl text-blue-600 border-red-900";
+
+// function App() {
+
+//   const numThings = 3;
+
+//   const wholeThing = <div className={bigRedCss}>
+//     Hello {name} !!!
+//   {button}
+//   </div>
+
+//   return numThings > 4 ? wholeThing : <div>404</div>
+// }
