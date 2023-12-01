b938e14b3711f7984c95f9c37870efe8455b9bb0
Steven Marcus <smarcus@cicsnc.org>
2023-12-01 11:17:09 -0500

    wip

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,39 @@
 import './App.css'
 
-const name="Steven Marcus";
-const button = <button>Click Me Fecker</button>;
-const bigRedCss = "text-4xl text-blue-600 border-red-900";
 
 function App() {
 
-  const numThings = 3;
+  return (
+    <div className="text-4xl  border rounded-lg border-red-800 bg-gray-200 p-2 m-2" >
+      Hello
+    </div>
 
-  const wholeThing = <div className={bigRedCss}>
-    Hello {name} !!!
-  {button}
-  </div>
-
-  return numThings > 4 ? wholeThing : <div>404</div>
+    )
 }
 
 export default App
+
+
+// ***** Works ***
+// const name="Steven Marcus";
+// const button = <button>Click Me Fecker</button>;
+// const bigRedCss = "text-4xl border rounded-lg p-2 m-2";
+
+// function App() {
+
+//   const numThings = 33;
+
+//   const wholeThing = <div className={bigRedCss}>
+//     Hello {name} !!!
+//   {button}
+//   </div>
+
+//   return numThings > 4 ? wholeThing : <div>404</div>
+// }
+
+
+
+
 
 // *** Weird because wholeThing doesn't use quotes ""
 // function App() {
